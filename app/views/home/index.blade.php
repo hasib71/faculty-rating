b48164d5d20fb00bfa4ec1f3f4f724bd0881dc37
Hasib Al Muhaimin <himuhasib@gmail.com>
2015-05-27 20:07:10 +0600

    Basic home page

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,9 +1,40 @@
 @extends('layouts.default')
 
 @section('title')
-	<title>Hello World!</title>
+	<title>Welcome To Faculty Rating System!</title>
 @stop
 
 @section('content')
-	<h1>Hello World!</h1>
+
+	<div class="container">
+		<center>
+			<h3>Choose Your Depertment</h3>
+			<hr/>
+		</center>
+
+		<div class="row">
+			<div class="col-xs-6">
+				<img src="http://www.northsouth.edu/images/business1.jpg">
+				<b>School of Business & Economics</b>
+			</div>
+			<div class="col-xs-6">
+				<img src="http://www.northsouth.edu/images/eas1.jpg">
+				<b>School of Engineering & Physical Sciences</b>
+			</div>
+		</div>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-xs-6">
+				<img src="http://www.northsouth.edu/images/orientation20131.jpg">
+				<b>School of Engineering & Physical Sciences</b>
+			</div>
+
+			<div class="col-xs-6">
+				<img src="http://www.northsouth.edu/images/lifescience1.jpg">
+				<b>School of Health & Life Sciences</b>
+			</div>
+		</div>
+
+	</div>
 @stop
